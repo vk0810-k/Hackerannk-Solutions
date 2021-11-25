198aef4a4f999f183fe820f6c6eb02e8c5feadfd
Varun Mahendra Khadayate <68016633+vk0810-k@users.noreply.github.com>
2021-11-25 23:26:09 +0530

    Update answer.c

--- Interview Preparation Kit/Warm-up Challenges/Sales by Match/answer.c
+++ Interview Preparation Kit/Warm-up Challenges/Sales by Match/answer.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+int main() {
+ int n;
+    scanf("%d",&n);
+ int a[1000] = {};
+ for(int i = 0; i < n; i++) {
+        int c;
+        scanf("%d",&c);
+        a[c]++;
+    }
+
+ int res = 0;
+ for(int i = 0; i <= 100; i++){
+         res += a[i] / 2;
+     }
+ printf("%d\n",res);
+ return 0;
+}

eff0d923167b52788bfb7c0e63a8fb1802401d02
Varun Mahendra Khadayate <68016633+vk0810-k@users.noreply.github.com>
2021-11-30 23:35:59 +0530

    Create Hash Tables: Ransom Note.py

---
+++ Interview Preparation Kit/Dictionaries and Hashmaps/Hash Tables: Ransom Note.py
@@ -0,0 +1,25 @@
+def checkMagazine(magazine, note):
+    magazine.sort()
+    note.sort()
+
+    for word in note:
+        if word not in magazine:
+            flag = False
+            break
+        else:
+            magazine.remove(word)
+        flag = True
+
+    if flag:
+        print("Yes")
+    else:
+        print("No")
+
+if __name__ == '__main__':
+    mn = input().split()
+    m = int(mn[0])
+    n = int(mn[1])
+
+    MAGAZINE = input().rstrip().split()
+    NOTE = input().rstrip().split()
+    checkMagazine(MAGAZINE, NOTE)

058defcdc06213f28e87f955bd916bddcfb00344
Varun Mahendra Khadayate <68016633+vk0810-k@users.noreply.github.com>
2021-11-30 23:14:08 +0530

    Create Arrays: Left Rotation.py

---
+++ Interview Preparation Kit/Arrays/Arrays: Left Rotation.py
@@ -0,0 +1,21 @@
+import os
+
+def rotLeft(a, d):
+    size = len(a)
+
+    if size < d:
+        d = size % d
+    return a[d:size] + a[0:d]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    nd = input().split()
+    n = int(nd[0])
+    d = int(nd[1])
+    a = list(map(int, input().rstrip().split()))
+
+    RESULT = rotLeft(a, d)
+    fptr.write(' '.join(map(str, RESULT)))
+    fptr.write('\n')
+    fptr.close()
